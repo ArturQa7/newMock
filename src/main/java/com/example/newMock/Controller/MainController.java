988d13d14ea90590b5b4c4e2bed1653480802e5e
ArturQa7 <iio4ka99@mail.ru>
2023-07-09 13:51:59 +0300

    second_commit

--- src/main/java/com/example/newMock/Controller/MainController.java
+++ src/main/java/com/example/newMock/Controller/MainController.java
@@ -2,10 +2,13 @@ package com.example.newMock.Controller;
 
 
 import com.example.newMock.Model.RequestDTO;
+import com.example.newMock.Model.ResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +32,32 @@ public class MainController {
             String clientid = requestDTO.getClientId();
             char firstDigit = clientid.charAt(0);
             BigDecimal maxLimit;
+
+            if (firstDigit == '8') {
+                maxLimit = new BigDecimal(2000);
+            } else if (firstDigit == '9') {
+                maxLimit = new BigDecimal(1000);
+            } else {
+                maxLimit = new BigDecimal(10000);
+            }
+
+            String RqUID = requestDTO.getRqUID(); //первый способ получения ID
+
+            ResponseDTO responseDTO = new ResponseDTO();
+
+            responseDTO.setRqUID(RqUID); //второй способ получения ID
+            responseDTO.setClientId(clientid);
+            responseDTO.setAccount(requestDTO.getAccount());
+            responseDTO.setCurrency("RUB");
+            responseDTO.setBalance("900");
+            responseDTO.setMaxLimit(maxLimit);
+
+            log.error("***** Запрос ***** " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
+            log.error("***** Запрос ***** " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+
+            return responseDTO
+      } catch (Exception e) {
+          return ResponseEntity.status((HttpStatus.BAD_REQUEST).body(e.getMessage()));
       }
 
     }
